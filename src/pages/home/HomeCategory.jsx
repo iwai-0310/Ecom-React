74aff9581c61c3bdc4fede6f728b6bbbddc34dd3
iwai-0310 <chazesingh@gmail.com>
2023-11-22 02:16:47 +0530

    Adds seciton header and cards to the homecatergory

--- src/pages/home/HomeCategory.jsx
+++ src/pages/home/HomeCategory.jsx
@@ -1,7 +1,82 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+const subTitle = "Choose Any Products";
+const title = "Buy Everything with Us";
+const btnText = "Get Started Now";
+
+const categoryList = [
+  {
+    imgUrl: "src/assets/images/category/01.jpg",
+    imgAlt: "category rajibraj91 rajibraj",
+    iconName: "icofont-brand-windows",
+    title: "DSLR Camera",
+  },
+  {
+    imgUrl: "src/assets/images/category/02.jpg",
+    imgAlt: "category rajibraj91 rajibraj",
+    iconName: "icofont-brand-windows",
+    title: "Shoes",
+  },
+  {
+    imgUrl: "src/assets/images/category/03.jpg",
+    imgAlt: "category rajibraj91 rajibraj",
+    iconName: "icofont-brand-windows",
+    title: "Photography",
+  },
+  {
+    imgUrl: "src/assets/images/category/04.jpg",
+    imgAlt: "category rajibraj91 rajibraj",
+    iconName: "icofont-brand-windows",
+    title: "Formal Dress",
+  },
+  {
+    imgUrl: "src/assets/images/category/05.jpg",
+    imgAlt: "category rajibraj91 rajibraj",
+    iconName: "icofont-brand-windows",
+    title: "Colorful Bags",
+  },
+  {
+    imgUrl: "src/assets/images/category/06.jpg",
+    imgAlt: "category rajibraj91 rajibraj",
+    iconName: "icofont-brand-windows",
+    title: "Home Decor",
+  },
+];
 
 const HomeCategory = () => {
-  return <div>HomeCategory</div>;
+  return (
+    <div className="category-section style-4 padding-tb">
+      <div className="container">
+        {/* section-header */}
+        <div className="section-header text-center">
+          <span className="subtitle">{subTitle}</span>
+          <h2 className="title">{title}</h2>
+        </div>
+
+        {/* section-card */}
+        <div className="section-wrapper">
+          <div className="grid gap-4 justify-center grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
+            {categoryList.map((val, i) => (
+              <div key={i} className="col">
+                <Link to="/shop" className="category-item">
+                  <div className="category-inner ">
+                    <div className="category-thumb">
+                      <img
+                        className="border-solid border-2 border-sky-800 "
+                        src={val.imgUrl}
+                        alt={val.imgAlt}
+                      />
+                    </div>
+                  </div>
+                </Link>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomeCategory;
