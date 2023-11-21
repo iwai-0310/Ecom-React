1c8e006cba83f80ec2142de4943b461809fb5bb7
iwai-0310 <chazesingh@gmail.com>
2023-11-22 03:00:16 +0530

    move the get-Started btn css to index.css

--- src/pages/home/HomeCategory.jsx
+++ src/pages/home/HomeCategory.jsx
@@ -92,12 +92,10 @@ const HomeCategory = () => {
               href="#_"
               class="relative inline-flex items-center justify-start inline-block px-5 py-3 overflow-hidden font-bold rounded-full group"
             >
-              <span class="w-32 h-32 rotate-45 translate-x-12 -translate-y-2 absolute left-0 top-0 bg-black opacity-[3%]"></span>
-              <span class="absolute top-0 left-0 w-48 h-48 -mt-1 transition-all duration-500 ease-in-out rotate-45 -translate-x-56 -translate-y-24 bg-black opacity-100 group-hover:-translate-x-8"></span>
-              <span class="relative w-full text-left text-black transition-colors duration-200 ease-in-out group-hover:text-gray-100">
-                {btnText}
-              </span>
-              <span class="absolute inset-0 border-2 border-black rounded-full"></span>
+              <span class="get-started-btn-span1"></span>
+              <span class="get-Started-btn-span2"></span>
+              <span class="get-Started-btn-span3">{btnText}</span>
+              <span class="get-Started-btn-span4"></span>
             </a>
           </Link>
         </div>
