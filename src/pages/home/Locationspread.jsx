aaaace9e6d88c82dd526bce0216f6f80817d2161
iwai-0310 <chazesingh@gmail.com>
2023-11-23 00:20:04 +0530

    adds client data to locationspread

--- src/pages/home/Locationspread.jsx
+++ src/pages/home/Locationspread.jsx
@@ -1,5 +1,48 @@
 import React from "react";
 
+const title = "More Then 10,000 Customers";
+
+const desc =
+  "Buy products on your any device with our app & enjoy your time what you want. Just download & install & start to shopping";
+
+const clientsList = [
+  {
+    imgUrl: "/src/assets/images/clients/avater.jpg",
+    imgAlt: "education thumb rajibraj91 rajibraj",
+    text: "Join with Us",
+  },
+  {
+    imgUrl: "/src/assets/images/clients/avater.jpg",
+    imgAlt: "education thumb rajibraj91 rajibraj",
+    text: "Join with Us",
+  },
+  {
+    imgUrl: "/src/assets/images/clients/avater.jpg",
+    imgAlt: "education thumb rajibraj91 rajibraj",
+    text: "Join with Us",
+  },
+  {
+    imgUrl: "/src/assets/images/clients/avater.jpg",
+    imgAlt: "education thumb rajibraj91 rajibraj",
+    text: "Join with Us",
+  },
+  {
+    imgUrl: "/src/assets/images/clients/avater.jpg",
+    imgAlt: "education thumb rajibraj91 rajibraj",
+    text: "Join with Us",
+  },
+  {
+    imgUrl: "/src/assets/images/clients/avater.jpg",
+    imgAlt: "education thumb rajibraj91 rajibraj",
+    text: "Join with Us",
+  },
+  {
+    imgUrl: "/src/assets/images/clients/avater.jpg",
+    imgAlt: "education thumb rajibraj91 rajibraj",
+    text: "Join with Us",
+  },
+];
+
 const Locationspread = () => {
   return <div>Locationspread</div>;
 };
