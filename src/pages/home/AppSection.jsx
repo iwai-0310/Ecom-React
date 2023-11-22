ffeda6334810b1e02536850b244c5aec74612f94
iwai-0310 <chazesingh@gmail.com>
2023-11-23 02:10:01 +0530

    Adds text title and desc to appsection

--- src/pages/home/AppSection.jsx
+++ src/pages/home/AppSection.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
+const btnText = "Sign up for free";
+const title = "Shop Anytime, Anywhere";
+const desc = "Shop Or Learn. Download & install the app to grow";
+
 const AppSection = () => {
   return <div>AppSection</div>;
 };
