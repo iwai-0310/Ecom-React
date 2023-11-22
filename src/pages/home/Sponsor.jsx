44a8092a1046fd09a0f8de0da899840e5fbfb5c1
iwai-0310 <chazesingh@gmail.com>
2023-11-23 02:24:23 +0530

    Adds imports for swiper for docs to sponsor

--- src/pages/home/Sponsor.jsx
+++ src/pages/home/Sponsor.jsx
@@ -1,4 +1,14 @@
 import React from "react";
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+// import required modules
+import { Pagination } from "swiper/modules";
 
 const sponsorList = [
   {
@@ -21,7 +31,15 @@ const sponsorList = [
   },
 ];
 const Sponsor = () => {
-  return <div>Sponsor</div>;
+  return (
+    <div className="sponsor-section section-bg">
+      <div className="container">
+        <div className="section-wrapper">
+          <div className="sponsor-slider"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Sponsor;
