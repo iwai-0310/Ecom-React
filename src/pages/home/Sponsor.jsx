20ffcdf5cbbc912d12bb11b97faa721ed8595e0a
iwai-0310 <chazesingh@gmail.com>
2023-11-23 02:20:18 +0530

    Adds sponsor data to sponsor

--- src/pages/home/Sponsor.jsx
+++ src/pages/home/Sponsor.jsx
@@ -1,5 +1,25 @@
 import React from "react";
 
+const sponsorList = [
+  {
+    imgUrl: "/src/assets/images/sponsor/01.png",
+  },
+  {
+    imgUrl: "/src/assets/images/sponsor/02.png",
+  },
+  {
+    imgUrl: "/src/assets/images/sponsor/03.png",
+  },
+  {
+    imgUrl: "/src/assets/images/sponsor/04.png",
+  },
+  {
+    imgUrl: "/src/assets/images/sponsor/05.png",
+  },
+  {
+    imgUrl: "/src/assets/images/sponsor/06.png",
+  },
+];
 const Sponsor = () => {
   return <div>Sponsor</div>;
 };
