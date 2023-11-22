d5f9010018045d707b46a5dd78e10ac2e557b366
iwai-0310 <chazesingh@gmail.com>
2023-11-22 17:18:30 +0530

    create and categoryshowcase to home

---
+++ src/pages/home/CategoryShowcase.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CategoryShowcase = () => {
+  return <div>CategoryShowcase</div>;
+};
+
+export default CategoryShowcase;
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "./Banner";
 import HomeCategory from "./HomeCategory";
+import CategoryShowcase from "./CategoryShowcase";
 
 export const home = () => {
   return (
     <div>
       <Banner />
       <HomeCategory />
+      <CategoryShowcase />
     </div>
   );
 };
