092aeebd1462a259f00c1f24f8c25b92fdfee1d1
iwai-0310 <chazesingh@gmail.com>
2023-11-23 02:13:44 +0530

    adds btn and title to render of appsection

--- src/pages/home/AppSection.jsx
+++ src/pages/home/AppSection.jsx
@@ -1,11 +1,23 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const btnText = "Sign up for free";
-const title = "Shop Anytime, Anywhere";
+const title = "Shop Anything ,Anytime, Anywhere";
 const desc = "Shop Or Learn. Download & install the app to grow";
 
 const AppSection = () => {
-  return <div>AppSection</div>;
+  return (
+    <div className="app-section py-8">
+      <div className="container">
+        <div className="section-header text-center">
+          <Link to="/sign-up" className="lab-btn mb-4">
+            {btnText}
+          </Link>
+          <h2 className="title">{title}</h2>
+          <p>{desc}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AppSection;
