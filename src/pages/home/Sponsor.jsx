c41fea4da2b05849fcd3c8e2690b6356517d2e10
iwai-0310 <chazesingh@gmail.com>
2023-11-23 02:31:43 +0530

    Adds imgs to swiper

--- src/pages/home/Sponsor.jsx
+++ src/pages/home/Sponsor.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import React, { useRef, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -8,7 +7,7 @@ import "swiper/css";
 import "swiper/css/pagination";
 
 // import required modules
-import { Pagination } from "swiper/modules";
+import { Autoplay, Pagination } from "swiper/modules";
 
 const sponsorList = [
   {
@@ -35,7 +34,42 @@ const Sponsor = () => {
     <div className="sponsor-section section-bg">
       <div className="container">
         <div className="section-wrapper">
-          <div className="sponsor-slider"></div>
+          <div className="sponsor-slider">
+            <Swiper
+              slidesPerView={2}
+              spaceBetween={20}
+              autoplay={{
+                delay: 2000,
+                disableOnInteraction: false,
+              }}
+              breakpoints={{
+                640: {
+                  slidesPerView: 2,
+                  spaceBetween: 20,
+                },
+                768: {
+                  slidesPerView: 4,
+                  spaceBetween: 40,
+                },
+                1024: {
+                  slidesPerView: 6,
+                  spaceBetween: 50,
+                },
+              }}
+              modules={[Autoplay]}
+              className="mySwiper"
+            >
+              {sponsorList.map((val, i) => (
+                <SwiperSlide key={i}>
+                  <div className="sponsor-item">
+                    <div className="sponsor-thumb">
+                      <img src={val.imgUrl} alt="" />
+                    </div>
+                  </div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
         </div>
       </div>
     </div>
