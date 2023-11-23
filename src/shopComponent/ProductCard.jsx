d7a9b850594b6c8c2a7046b979fb98510d0a3d56
iwai-0310 <chazesingh@gmail.com>
2023-11-24 01:51:05 +0530

    fiex the prop declaration error

--- src/shopComponent/ProductCard.jsx
+++ src/shopComponent/ProductCard.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
-const ProductCard = () => {
-  return <div>ProductCard</div>;
+const ProductCard = ({ GridList, product }) => {
+  return (
+    <div
+      className={`shop-product-wrap flex justify-center ${
+        GridList ? "grid" : "list"
+      }`}
+    >
+      {product.map((product, i) => (
+        <div className="lg:w-4/12 md:w-6/12 w-full"></div>
+      ))}
+    </div>
+  );
 };
 
 export default ProductCard;
