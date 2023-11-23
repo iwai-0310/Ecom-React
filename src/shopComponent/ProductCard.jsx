5e3cebd43155bbec6afac9df60c97537d6b3e193
iwai-0310 <chazesingh@gmail.com>
2023-11-24 01:56:38 +0530

    Add icons and imgs to product card

--- src/shopComponent/ProductCard.jsx
+++ src/shopComponent/ProductCard.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ProductCard = ({ GridList, product }) => {
   return (
@@ -8,7 +9,28 @@ const ProductCard = ({ GridList, product }) => {
       }`}
     >
       {product.map((product, i) => (
-        <div className="lg:w-4/12 md:w-6/12 w-full"></div>
+        <div className="lg:w-4/12 md:w-6/12 w-full">
+          <div className="product-item">
+            {/* product images here */}
+            <div className="product-thumb">
+              <div className="pro-thumb">
+                <img src={product.img} alt="" />
+              </div>
+              {/* product action link */}
+              <div className="product-action-link">
+                <Link to={`/shop/${product.id}`}>
+                  <i className="icofont-eye"></i>
+                </Link>
+                <a href="#">
+                  <i className="icofont-heart"></i>
+                </a>
+                <Link to="/cart-page">
+                  <i className="icofont-cart-alt"></i>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
       ))}
     </div>
   );
