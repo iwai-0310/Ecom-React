2be3413980181872ccf31f8c15e48081ee7b97b1
iwai-0310 <chazesingh@gmail.com>
2023-11-24 01:37:43 +0530

    Add results pageination title and two icons for grid/list view

--- src/shopComponent/shop.jsx
+++ src/shopComponent/shop.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import PageHeader from "../components/PageHeader";
 
+const showResults = "Showing 01 - 12 of 139 Results";
+
 const shop = () => {
+  const [GridList, setGridList] = useState(true);
   return (
     <div className="">
       <PageHeader title="Our Shop page" curPage="Shop" />
@@ -9,7 +12,33 @@ const shop = () => {
       <div className="shop-page py-8">
         <div className="container">
           <div className="flex justify-center">
-            <div className="lg:w-8/12 w-full">left side</div>
+            {/* left-section for items view */}
+            <div className="lg:w-8/12 w-full">
+              <article>
+                <div className="shop-title flex flex-wrap justify-between">
+                  <p>{showResults}</p>
+                  <div
+                    className={`flex items-center product-view-mode ${
+                      GridList ? "gridActive" : "ListActive"
+                    }`}
+                  >
+                    <button
+                      className="grid"
+                      onClick={() => setGridList(!GridList)}
+                    >
+                      <i className="icofont-ghost"></i>
+                    </button>
+                    <button
+                      className="list"
+                      onClick={() => setGridList(!GridList)}
+                    >
+                      <i className="icofont-listine-dots"></i>
+                    </button>
+                  </div>
+                </div>
+              </article>
+            </div>
+            {/* right-section for sort and other functionalities */}
             <div className="lg:w-4/12 w-full">right side</div>
           </div>
         </div>
