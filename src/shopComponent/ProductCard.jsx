39a7f6d5951764b0fd65c26d1cf753b005c43bb7
iwai-0310 <chazesingh@gmail.com>
2023-11-24 02:54:30 +0530

    some cosmetics to product card

--- src/shopComponent/ProductCard.jsx
+++ src/shopComponent/ProductCard.jsx
@@ -42,6 +42,39 @@ const ProductCard = ({ GridList, product }) => {
               <h6>$ {product.price}</h6>
             </div>
           </div>
+
+          {/* list view */}
+          <div className="product-list-item">
+            {/* product images here */}
+            <div className="product-thumb">
+              <div className="pro-thumb">
+                <img src={product.img} alt="" />
+              </div>
+              {/* product action link */}
+              <div className="product-action-link">
+                <Link to={`/shop/${product.id}`}>
+                  <i className="icofont-eye"></i>
+                </Link>
+                <a href="#">
+                  <i className="icofont-heart"></i>
+                </a>
+                <Link to="/cart-page">
+                  <i className="icofont-cart-alt"></i>
+                </Link>
+              </div>
+            </div>
+
+            {/* product details */}
+            <div className="product-content">
+              <h5>
+                <Link to={`/shop/${product.id}`}>{product.name}</Link>
+              </h5>
+              <p className="product-rating">
+                <Rating />
+              </p>
+              <h6>$ {product.price}</h6>
+            </div>
+          </div>
         </div>
       ))}
     </div>
