7d7e8f56c7c0e6f7ade10c409d4e9477cbc224af
iwai-0310 <chazesingh@gmail.com>
2023-11-24 17:00:58 +0530

    Adds search input and button to  right side of shop

--- src/shopComponent/Search.jsx
+++ src/shopComponent/Search.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Search = ({ product, GridList }) => {
-  return <div>Search</div>;
+  const [searchTerm, setSearchTerm] = useState(" ");
+  const filteredProducts = product.filter((item) =>
+    item.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div className="widget widget-search">
+      <form className="search-wrapper mb-3">
+        <input
+          type="text"
+          name="search"
+          id="search"
+          placeholder="Search ..."
+          defaultValue={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <button type="submit">
+          <i className="icofont-search-2"></i>
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Search;
