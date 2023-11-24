3f565a5eff2efadf33ccfd86efe495857acc1066
iwai-0310 <chazesingh@gmail.com>
2023-11-24 16:41:03 +0530

    fix error on pagination prop declaration

--- src/shopComponent/Pagination.jsx
+++ src/shopComponent/Pagination.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 
-const Pagination = (productsPerPage, totalProducts, paginate, activePage) => {
+const Pagination = ({
+  productsPerPage,
+  totalProducts,
+  paginate,
+  activePage,
+}) => {
   const pageNumbers = [];
   for (let i = 1; i <= Math.ceil(totalProducts / productsPerPage); i++) {
     pageNumbers.push(i);
   }
-  return <div>Pagination</div>;
+  return (
+    <ul>
+      {pageNumbers.map((number) => (
+        <li key={number}>
+          <a href="#">{number}</a>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default Pagination;
