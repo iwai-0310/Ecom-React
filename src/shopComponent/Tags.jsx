e00a5c323e0ceba0927651a66f3df2b58b73096f
iwai-0310 <chazesingh@gmail.com>
2023-11-27 00:54:09 +0530

    Creates and add data to tags comp

--- src/shopComponent/Tags.jsx
+++ src/shopComponent/Tags.jsx
@@ -1,5 +1,46 @@
 import React from "react";
 
+const title = "Our Popular Tags";
+
+const tagsList = [
+  {
+    link: "#",
+    text: "envato",
+  },
+  {
+    link: "#",
+    text: "themeforest",
+  },
+  {
+    link: "#",
+    text: "codecanyon",
+  },
+  {
+    link: "#",
+    text: "videohive",
+  },
+  {
+    link: "#",
+    text: "audiojungle",
+  },
+  {
+    link: "#",
+    text: "3docean",
+  },
+  {
+    link: "#",
+    text: "envato",
+  },
+  {
+    link: "#",
+    text: "themeforest",
+  },
+  {
+    link: "#",
+    text: "codecanyon",
+  },
+];
+
 const Tags = () => {
   return <div>Tags</div>;
 };
