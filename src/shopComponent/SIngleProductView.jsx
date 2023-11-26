99288621f43eea06ebc0571a9e4b30e01c71fd67
iwai-0310 <chazesingh@gmail.com>
2023-11-27 01:44:57 +0530

    Add logic for single product from all products

--- src/shopComponent/SIngleProductView.jsx
+++ src/shopComponent/SIngleProductView.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const SIngleProductView = () => {
-  return <div>SIngleProductView</div>;
+  const [product, setProduct] = useState([]);
+  //access the product id
+  const { id } = useParams();
+  //access all the products data
+  useEffect(() => {
+    fetch("/src/products.json")
+      .then((res) => res.json())
+      .then((data) => setProduct(data));
+  }, []);
+  //filter out the data with product.id
+  const result = product.filter((p) => p.id === id);
+  console.log(result);
+
+  return <div></div>;
 };
 
 export default SIngleProductView;
