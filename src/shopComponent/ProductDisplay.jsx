4e1d18741023b605f6678e159daf117623aa496e
iwai-0310 <chazesingh@gmail.com>
2023-11-27 21:15:31 +0530

    add details to product display and a dropdown for size

--- src/shopComponent/ProductDisplay.jsx
+++ src/shopComponent/ProductDisplay.jsx
@@ -1,8 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ProductDisplay = ({ item }) => {
-  const { name, id, price, seller, ratingCount } = item;
-  return <div></div>;
+  const { name, id, price, seller, ratingsCount, quantity } = item;
+
+  const [preQuantity, setQuantity] = useState(quantity);
+  const [coupon, setCoupon] = useState("");
+  const [size, setSize] = useState("Select Sizes");
+  const [color, setColor] = useState("Select Color");
+
+  const handleSizeChange = (event) => {
+    setSize(event.target.value);
+  };
+  return (
+    <div>
+      <div>
+        <h4>{name}</h4>
+        <p className="rating">
+          <i className="icofont-star"></i>
+          <i className="icofont-star"></i>
+          <i className="icofont-star"></i>
+          <i className="icofont-star"></i>
+          <i className="icofont-star"></i>
+          <span>{ratingsCount} review</span>
+        </p>
+        <h4>${price}</h4>
+        <h6>{seller}</h6>
+        {/* Add product desc below */}
+      </div>
+      {/* cart component */}
+      <div>
+        <form>
+          <div className="select-product Size">
+            <select value={size} onChange={handleSizeChange}>
+              <option value="NaN">select size</option>
+              <option value="XS">XS</option>
+              <option value="S">S</option>
+              <option value="M">M</option>
+              <option value="L">L</option>
+              <option value="XL">XL</option>
+            </select>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default ProductDisplay;
