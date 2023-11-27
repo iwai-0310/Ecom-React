86c6aedb4ac65f576bfd71e246543c9dcf49783b
iwai-0310 <chazesingh@gmail.com>
2023-11-28 03:19:21 +0530

    add reviews to the review-section under product view

--- src/shopComponent/Review.jsx
+++ src/shopComponent/Review.jsx
@@ -37,6 +37,7 @@ const Review = () => {
   const [reviewShow, setReviewShow] = useState(true);
   return (
     <>
+      {/* Desc and review buttons */}
       <ul
         className={`review-nav lab-ul ${
           reviewShow ? "RevActive" : "DescActive"
@@ -49,6 +50,36 @@ const Review = () => {
           Reviews 4
         </li>
       </ul>
+      {/* Desc and review contents */}
+      <div
+        className={`review-content ${
+          reviewShow ? "review-content-show" : "description-show"
+        }`}
+      >
+        <div className="review-showing">
+          <ul className="content lab-ul">
+            {ReviewList.map((review, i) => (
+              <li key={i}>
+                <div className="post-thumb">
+                  <img src={review.imgUrl} alt="" />
+                </div>
+                <div className="post-content">
+                  <div className="entry-meta">
+                    <div className="posted-on">
+                      <a href="#">{review.name}</a>
+                      <p>{review.date}</p>
+                    </div>
+                  </div>
+                  <div className="entry-content">
+                    <p>{review.desc}</p>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+        {/* description content */}
+      </div>
     </>
   );
 };
