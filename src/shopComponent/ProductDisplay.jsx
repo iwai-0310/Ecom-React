fa8cf54e2b594845171a3ea12a7d2507cd690914
iwai-0310 <chazesingh@gmail.com>
2023-11-27 21:28:06 +0530

    add inc and desc for quantity

--- src/shopComponent/ProductDisplay.jsx
+++ src/shopComponent/ProductDisplay.jsx
@@ -32,30 +32,44 @@ const ProductDisplay = ({ item }) => {
       </div>
       {/* cart component */}
       <div>
-        <form className="flex justify-between">
-          {/* dropdown for size */}
-          <div className="select-product size">
-            <select value={size} onChange={handleSizeChange}>
-              <option value="NaN">select size</option>
-              <option value="XS">XS</option>
-              <option value="S">S</option>
-              <option value="M">M</option>
-              <option value="L">L</option>
-              <option value="XL">XL</option>
-            </select>
-            <i className="icofont-rounder-down"></i>
+        <form>
+          <div className="flex justify-between my-4">
+            {/* dropdown for size */}
+            <div className="select-product size w-5/12">
+              <select value={size} onChange={handleSizeChange}>
+                <option value="NaN">select size</option>
+                <option value="XS">XS</option>
+                <option value="S">S</option>
+                <option value="M">M</option>
+                <option value="L">L</option>
+                <option value="XL">XL</option>
+              </select>
+              <i className="icofont-rounder-down"></i>
+            </div>
+            {/* dropdown for color */}
+            <div className="select-product color w-6/12">
+              <select value={color} onChange={handleColorChange}>
+                <option value="Violet">Violet</option>
+                <option value="Indigo">Indigo</option>
+                <option value="Blue">Blue</option>
+                <option value="Green">Green</option>
+                <option value="White">White</option>
+                <option value="Black">Black</option>
+              </select>
+              <i className="icofont-rounder-down"></i>
+            </div>
           </div>
-          {/* dropdown for color */}
-          <div className="select-product color">
-            <select value={color} onChange={handleColorChange}>
-              <option value="Violet">Violet</option>
-              <option value="Indigo">Indigo</option>
-              <option value="Blue">Blue</option>
-              <option value="Green">Green</option>
-              <option value="White">White</option>
-              <option value="Black">Black</option>
-            </select>
-            <i className="icofont-rounder-down"></i>
+          {/* cart quantity */}
+          <div className="cart-plus-minus">
+            <div className="dec qtybutton">-</div>
+            <input
+              className="cart-plus-minus-box"
+              type="text"
+              name="qtybutton"
+              id="qtybutton"
+              value={preQuantity}
+            />
+            <div className="inc qtybutton">+</div>
           </div>
         </form>
       </div>
