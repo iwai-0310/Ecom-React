e783e6fd2146dab68ea29509bd3d81f87cbbbbc2
iwai-0310 <chazesingh@gmail.com>
2023-11-27 21:48:55 +0530

    Adds button for cart and checkout

--- src/shopComponent/ProductDisplay.jsx
+++ src/shopComponent/ProductDisplay.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const ProductDisplay = ({ item }) => {
   const { name, id, price, seller, ratingsCount, quantity } = item;
@@ -99,6 +100,18 @@ const ProductDisplay = ({ item }) => {
                 onChange={(e) => setCoupon(e.target.value)}
               />
             </div>
+          </div>
+          {/* buttons section */}
+          <div className="flex justify-between">
+            <button type="submit" className="lab-btn ">
+              Add to Cart
+            </button>
+            <Link
+              to="/cart-page"
+              className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+            >
+              Check out
+            </Link>
           </div>
         </form>
       </div>
