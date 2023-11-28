e0bd3c8451ef240da9f2cea2faa04e922534d223
iwai-0310 <chazesingh@gmail.com>
2023-11-28 20:20:23 +0530

    Adds quantity incr and dec while acessing the local storage for cart item

--- src/shopComponent/CartPage.jsx
+++ src/shopComponent/CartPage.jsx
@@ -1,6 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
 
 const CartPage = () => {
+  const [cartItems, setCartItems] = useState([]);
+  useEffect(() => {
+    //fetch cart item from local storage
+    const storedCartItems = JSON.parse(localStorage.getItem("cart")) || [];
+    setCartItems(storedCartItems);
+  }, []);
+
+  //calaculate total price
+  const calcTotalPrice = (item) => {
+    return item.price * item.quantity;
+  };
+  //handle quantity increase
+  const handleIncrease = (item) => {
+    item.quantity += 1;
+    setCartItems([...cartItems]);
+
+    //update the local storage with new cart items
+    localStorage.setItem("cart", JSON.stringify(cartItems));
+  };
+
+  //handle quantity decrease
+  const handleDecrease = (item) => {
+    if (item.quantity > 1) {
+      item.quantity -= 1;
+    }
+    setCartItems([...cartItems]);
+
+    //update the local storage with new cart items
+    localStorage.setItem("cart", JSON.stringify(cartItems));
+  };
   return <div>CartPage</div>;
 };
 
