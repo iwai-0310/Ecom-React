f1340d635ece037aa122b776a7fabc0c530a404a
iwai-0310 <chazesingh@gmail.com>
2023-11-28 20:36:02 +0530

    Adds thead to the cart

--- src/shopComponent/CartPage.jsx
+++ src/shopComponent/CartPage.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
+import PageHeader from "../components/PageHeader";
 
 const CartPage = () => {
   const [cartItems, setCartItems] = useState([]);
@@ -13,7 +14,8 @@ const CartPage = () => {
   const calcTotalPrice = (item) => {
     return item.price * item.quantity;
   };
-  //handle quantity increase
+
+  //function tohandle quantity increase
   const handleIncrease = (item) => {
     item.quantity += 1;
     setCartItems([...cartItems]);
@@ -22,17 +24,61 @@ const CartPage = () => {
     localStorage.setItem("cart", JSON.stringify(cartItems));
   };
 
-  //handle quantity decrease
+  //function to handle quantity decrease
   const handleDecrease = (item) => {
     if (item.quantity > 1) {
       item.quantity -= 1;
-    }
-    setCartItems([...cartItems]);
 
-    //update the local storage with new cart items
-    localStorage.setItem("cart", JSON.stringify(cartItems));
+      setCartItems([...cartItems]);
+
+      //update the local storage with new cart items
+      localStorage.setItem("cart", JSON.stringify(cartItems));
+    }
   };
-  return <div>CartPage</div>;
+  // function to handle remove item
+  const handleRemoveItem = (item) => {
+    const updatedCart = cartItems.filter((cartItem) => cartItem.id !== item.id);
+    //update the cart
+    setCartItems(updatedCart);
+    updateLocalStorage(updatedCart);
+  };
+  // function to update local storage
+  const updateLocalStorage = (cart) => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  };
+  //cal cart subtotal
+  const cartSubTotal = cartItems.reduce((total, item) => {
+    return total + calcTotalPrice(item);
+  }, 0);
+
+  //cal order total
+  const orderTotal = cartSubTotal;
+  return (
+    <div>
+      <PageHeader title={"Shop cart"} curPage={"Cart Page"} />
+      <div className="shop-cart py-8">
+        <div className="container">
+          <div className="section-wrapper">
+            {/* cart top */}
+            <div className="cart-top">
+              <table>
+                <thead className="justify-between">
+                  <tr>
+                    <th className="cat-product">Product</th>
+                    <th className="cat-price">Price</th>
+                    <th className="cat-quantity">Quantity</th>
+                    <th className="cat-total">Total</th>
+                    <th className="cat-edit">Edit</th>
+                  </tr>
+                </thead>
+              </table>
+            </div>
+            {/* cart top ends */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CartPage;
