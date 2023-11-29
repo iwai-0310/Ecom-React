7b97a46f85960278e9e6af5c23105953da576536
iwai-0310 <chazesingh@gmail.com>
2023-11-29 22:37:29 +0530

    Adds a sample dialog for checkout

--- src/shopComponent/CheckoutPage.jsx
+++ src/shopComponent/CheckoutPage.jsx
@@ -1,23 +1,72 @@
 import React, { useState } from "react";
-import { Button } from "@material-tailwind/react";
+
+import {
+  Button,
+  Dialog,
+  DialogHeader,
+  DialogBody,
+  DialogFooter,
+} from "@material-tailwind/react";
 
 const CheckoutPage = () => {
   // state to show the pop up
-  // const [show, setShow] = useState(false);
-  // //state to show different banking options
-  // const [activeTab, setActiveTab] = useState("visa");
+  const [show, setShow] = useState(false);
+  //state to show different banking options
+  const [activeTab, setActiveTab] = useState("visa");
 
-  // //handle tab change
-  // const handleTabChange = (tabId) => {
-  //   setActiveTab(tabId);
-  // };
-  // const handleShow = () => {
-  //   setShow(true);
-  // };
-  // const handleHide = () => {
-  //   setShow(false);
-  // };
-  return <Button>material tailwind</Button>;
+  //handle tab change
+  const handleTabChange = (tabId) => {
+    setActiveTab(tabId);
+  };
+  const handleShow = () => {
+    setShow(true);
+  };
+  const handleHide = () => {
+    setShow(false);
+  };
+
+  return (
+    <div>
+      <Button onClick={handleShow} variant="gradient">
+        Proceed to Checkout
+      </Button>
+      ;
+      <Dialog open={show} handler={handleShow}>
+        <DialogHeader>
+          <h5>Select your payment option</h5>
+          <br className="text-gray-900" />
+        </DialogHeader>
+        <DialogBody>
+          <div className="mt-3 ">
+            <ul className="nav" id="myTab" role="tablist">
+              <li className="nav-item" role="presentation">
+                <a href="#visa">
+                  <img
+                    src="https://i.imgur.com/sB4jftM.png"
+                    alt=""
+                    width="80"
+                  />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </DialogBody>
+        <DialogFooter>
+          <Button
+            variant="text"
+            color="red"
+            onClick={handleHide}
+            className="mr-1"
+          >
+            <span>Cancel</span>
+          </Button>
+          <Button variant="gradient" color="green" onClick="">
+            <span>Confirm</span>
+          </Button>
+        </DialogFooter>
+      </Dialog>
+    </div>
+  );
 };
 
 export default CheckoutPage;
