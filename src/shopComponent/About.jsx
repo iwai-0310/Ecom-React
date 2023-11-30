58f87dee62b9a050f8dc996aca28bd540177912c
iwai-0310 <chazesingh@gmail.com>
2023-12-01 00:56:17 +0530

    Adds img with experience to about

--- src/shopComponent/About.jsx
+++ src/shopComponent/About.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PageHeader from "../components/PageHeader";
 
 const subTitle = "About Our Brand";
 const title = "Good Qualification Services And Better Expriences";
@@ -6,7 +7,7 @@ const desc =
   "Distinctively provide acces mutfuncto users whereas transparent proceses somes ncentivize eficient functionalities rather than extensible archtectur communicate leveraged services and cross-platform.";
 
 const year = "30+";
-const expareance = "Years Of Experiences";
+const experience = "Years Of Experiences";
 
 const aboutList = [
   {
@@ -30,7 +31,31 @@ const aboutList = [
 ];
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <PageHeader title={"About Our Brand"} curPage={"About"} />
+      <div className="about-section style-3 py-4 section-bg">
+        <div className="container">
+          <div className="grid justify-center lg:grid-cols-2 grid-cols-1 items-center">
+            <div className="col-span-1">
+              <div className="about-left">
+                <div className="about-thumb">
+                  <img src="/src/assets/images/about/01.jpg" alt="" />
+                </div>
+                {/* <div className="abs-thumb ">
+                  <img src="/src/assets/images/about/02.jpg" alt="" />
+                </div> */}
+                <div className="about-left-content">
+                  <h3>{year}</h3>
+                  <p>{experience}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
