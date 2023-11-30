9291549b54ccfd8fcca50514c779b1d5748281c2
iwai-0310 <chazesingh@gmail.com>
2023-11-30 16:11:55 +0530

    map the result to render img on the left

--- src/shopComponent/BlogView.jsx
+++ src/shopComponent/BlogView.jsx
@@ -16,8 +16,32 @@ const BlogView = () => {
       <div className="blog-section blog-single py-4 section-bg">
         <div className="container">
           <div className="flex justify-center">
-            <div className="lg:w-8/12 w-12">left</div>
+            {/* Left side starts */}
+            <div className="lg:w-8/12 w-12">
+              <article>
+                <div className="section-wrapper">
+                  <div className="grid grid-cols-1 justify-center gap-4 ">
+                    <div className="col-span-1">
+                      <div className="post-item style-2">
+                        <div className="post-inner">
+                          {result.map((item) => (
+                            <div key={item.id}>
+                              <div className="post-thumb">
+                                <img src={item.imgUrl} alt="" />
+                              </div>
+                            </div>
+                          ))}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </article>
+            </div>
+            {/* Left side ends */}
+            {/* Right side starts */}
             <div className="lg:w-4/12 w-12">right</div>
+            {/* Right Side ends */}
           </div>
         </div>
       </div>
