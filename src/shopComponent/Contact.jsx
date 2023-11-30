c351df740e96c6735aa84a414620ec59df4e3d2e
iwai-0310 <chazesingh@gmail.com>
2023-12-01 01:07:38 +0530

    Adds rel data to conteact page

--- src/shopComponent/Contact.jsx
+++ src/shopComponent/Contact.jsx
@@ -1,7 +1,53 @@
 import React from "react";
+import PageHeader from "../components/PageHeader";
+
+const subTitle = "Get in touch with us";
+const title = "We're Always Eager To Hear From You!";
+const conSubTitle = "Get in touch with Contact us";
+const conTitle =
+  "Fill The Form Below So We Can Get To Know You And Your Needs Better.";
+const btnText = "Send our Message";
+
+const contactList = [
+  {
+    imgUrl: "/src/assets/images/icon/01.png",
+    imgAlt: "contact icon",
+    title: "Office Address",
+    desc: "1201 park street, Fifth Avenue",
+  },
+  {
+    imgUrl: "/src/assets/images/icon/02.png",
+    imgAlt: "contact icon",
+    title: "Phone number",
+    desc: "+22698 745 632,02 982 745",
+  },
+  {
+    imgUrl: "/src/assets/images/icon/03.png",
+    imgAlt: "contact icon",
+    title: "Send email",
+    desc: "admin@shopcart.com",
+  },
+  {
+    imgUrl: "/src/assets/images/icon/04.png",
+    imgAlt: "contact icon",
+    title: "Our website",
+    desc: "www.shopcart.com",
+  },
+];
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div>
+      <PageHeader title={"Get in touch with us"} curPage={"Contact Us"} />
+      <div className="map-address-section py-3 section-bg">
+        <div className="container">
+          <div className="section-header text-center">
+            <span></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
