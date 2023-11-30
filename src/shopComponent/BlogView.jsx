9af5ea5c9c0a940e2c696d8e2d853723f1af0d56
iwai-0310 <chazesingh@gmail.com>
2023-11-30 16:07:37 +0530

    split the render area into left and right for blogview

--- src/shopComponent/BlogView.jsx
+++ src/shopComponent/BlogView.jsx
@@ -1,12 +1,28 @@
 import React, { useState } from "react";
 import blogList from "../utilis/blogdata";
 import { useParams } from "react-router-dom";
+import PageHeader from "../components/PageHeader";
 
 const BlogView = () => {
   const [blog, setBlog] = useState(blogList);
+  //get the id from url
   const { id } = useParams();
-  console.log(id);
-  return <div>BlogView</div>;
+  // console.log(id);
+  //access the blog data using the id
+  const result = blog.filter((b) => b.id === Number(id));
+  return (
+    <div>
+      <PageHeader title={"Single Blog Page"} curPage={"Blog/Blog Details"} />
+      <div className="blog-section blog-single py-4 section-bg">
+        <div className="container">
+          <div className="flex justify-center">
+            <div className="lg:w-8/12 w-12">left</div>
+            <div className="lg:w-4/12 w-12">right</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BlogView;
