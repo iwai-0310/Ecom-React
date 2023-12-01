861f350169bfceee974087e6340e69f187d1f398
iwai-0310 <chazesingh@gmail.com>
2023-12-02 02:50:32 +0530

    Adds rel data to login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,36 @@
 import React from "react";
 
+const title = "login";
+const socialTitle = "Login with Social Media";
+const buttonTxt = "Login Now";
+const socialList = [
+  {
+    iconName: "icofont-facebook",
+    siteLink: "#",
+    className: "facebook",
+  },
+  {
+    iconName: "icofont-twitter",
+    siteLink: "#",
+    className: "twitter",
+  },
+  {
+    iconName: "icofont-linkedin",
+    siteLink: "#",
+    className: "linkedin",
+  },
+  {
+    iconName: "icofont-instagram",
+    siteLink: "#",
+    className: "instagram",
+  },
+  {
+    iconName: "icofont-pinterest",
+    siteLink: "#",
+    className: "pinterest",
+  },
+];
+
 const Login = () => {
   return <div>Login</div>;
 };
