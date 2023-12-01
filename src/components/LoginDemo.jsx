61e617da254c855dd8083ad346ca3c9eff9c7f72
iwai-0310 <chazesingh@gmail.com>
2023-12-01 21:52:48 +0530

    Adds a function to access authentication from firebase for login

--- src/components/LoginDemo.jsx
+++ src/components/LoginDemo.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAuth } from "firebase/auth";
+
+const provider = new GoogleAuthProvider();
+const auth = getAuth();
 
 const LoginDemo = () => {
-  return <div>LoginDemo</div>;
+  // create a login method
+  const handleLogin = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        alert("Login Success Occured !");
+      })
+      .catchError((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.meessage;
+        console.log(error.message);
+      });
+  };
+  return (
+    <div className="m-5 p-10">
+      <button onClick={handleLogin} class="bg-green-300 text-gray-900">
+        Logme in
+      </button>
+    </div>
+  );
 };
 
 export default LoginDemo;
