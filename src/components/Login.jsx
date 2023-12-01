84927d542ba57e60f83983b882066a4d4cd4bba8
iwai-0310 <chazesingh@gmail.com>
2023-12-02 03:00:25 +0530

    Adds login form to login page with just email and password sectin

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { AuthContext } from "../contexts/AuthProvider";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const title = "login";
 const socialTitle = "Login with Social Media";
@@ -32,7 +34,46 @@ const socialList = [
 ];
 
 const Login = () => {
-  return <div>Login</div>;
+  const [error, setError] = useState("");
+
+  const { signupWithGmail, login } = useContext(AuthContext);
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const handleLogin = (e) => {};
+
+  const from = location.state?.from?.pathname || "/";
+  return (
+    <div>
+      <div className="login-section py-4 section-bg">
+        <div className="container">
+          <div className="account-wrapper">
+            <h3 className="title">{title}</h3>
+            <form className="account-form" onSubmit={handleLogin}>
+              <div className="form-group">
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="Email Address *"
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="Password here ..."
+                  required
+                />
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
