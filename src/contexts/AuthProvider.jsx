2746baa2a2a23a075547dd18307eccbe253a990b
iwai-0310 <chazesingh@gmail.com>
2023-12-01 22:30:30 +0530

    Adds state and googleAuthprovider to authprovider

--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -1,8 +1,14 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
+import app from "../firebase/firebase.config";
+import { GoogleAuthProvider, getAuth } from "firebase/auth";
 
 const AuthContext = createContext();
+const auth = getAuth();
+const googleProvider = new GoogleAuthProvider();
 const AuthProvider = ({ children }) => {
-  const authInfo = {};
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const authInfo = { user, loading };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
