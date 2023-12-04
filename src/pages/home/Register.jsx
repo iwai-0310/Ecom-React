51f88e94e6bea49851d1fa08a0c93d3219b31a41
iwai-0310 <chazesingh@gmail.com>
2023-12-05 04:19:11 +0530

    remove all the style from the register

--- src/pages/home/Register.jsx
+++ src/pages/home/Register.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const subTitle = "Save Your Day";
 const Title = (
-  <h2 className="title">
+  <h2>
     Join us on our day Long Free Workshop for <b>Advance </b>{" "}
     <span>Mastering </span> on Sales
   </h2>
@@ -58,7 +58,50 @@ const Register = () => {
     //   </section>
     // </div>
     <div>
-      <section className="bg-gradient-to-b from-gray-700 via-gray-400 to-white w-screen h-screen"></section>
+      <section className="bg-gradient-to-b from-gray-700 via-gray-400 to-white w-screen h-screen">
+        <div className="Registration-section">
+          <div className="grid gap-4 lg:grid-cols-2 grid-cols-1 items-center">
+            <div className="col-span-1">
+              <div className="Register-header">
+                <span className="Register-header-subtitle">{subTitle}</span>
+                {Title}
+                <p>{desc}</p>
+              </div>
+            </div>
+
+            <div>
+              <div className="Register-wrapper">
+                <h4>Register Now</h4>
+                <form className="Register-wrapper-form">
+                  <input
+                    type="text"
+                    name="name"
+                    placeholder="Username"
+                    className=""
+                  />
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    className=""
+                  />
+                  <input
+                    type="text"
+                    name="numer"
+                    pattern="[0-9]*"
+                    inputmode="numberic"
+                    placeholder="Phone no."
+                    className=""
+                  />
+                  <button type="submit" className="">
+                    Register
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
